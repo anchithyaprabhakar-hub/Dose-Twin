566105063c995711715fd8477f38f43441dccbf1
Anchithya Prabhakar <anchithyaprabhakar@gmail.com>
2026-07-12 23:00:29 +0530

    Create professional React project structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,7 @@
+import Home from "./pages/Home";
+
 function App() {
-  return (
-    <div className="min-h-screen bg-slate-900 flex items-center justify-center">
-      <h1 className="text-6xl font-bold text-cyan-400">
-        DoseTwin
-      </h1>
-    </div>
-  );
+  return <Home />;
 }
 
 export default App;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+function Home() {
+  return (
+    <div className="min-h-screen bg-slate-900 flex items-center justify-center">
+      <h1 className="text-6xl font-bold text-cyan-400">
+        DoseTwin
+      </h1>
+    </div>
+  );
+}
+
+export default Home;
